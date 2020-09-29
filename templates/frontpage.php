60d51ea97b40eafa036270f962d0a13d724497e2
[Adedipupo] <[jokanolaadedipupo@gmail.com]>
2020-09-29 15:26:18 +0100

    wip

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -5,9 +5,15 @@
   <!-- Main jumbotron for a primary marketing message or call to action -->
   <div class="container"> 
    <div class="jumbotron"> 
-      <h1 class="display-3">Hello, world!</h1>
-      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+      <h1>Find a Job</h1>
+      <form method="GET" action="index.php">
+        <select name="category" class="form-control">
+          <option value="0">Choose Category</option>
+          <?php foreach($categories as $category) : ?>
+            <option value="<?php echo $category->id;?><?php echo $category->name;?>"></option>
+          <?php endforeach; ?>  
+        </select>
+      </form>
     </div>
   </div>
 
